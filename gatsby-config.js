d13de98a77b5da4124e1f16ce7807c90bcbbdc58
utkarsh867 <utkarsh867@gmail.com>
2019-01-25 02:36:45 +0800

    correct pwa details

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `RC Tech`,
+        short_name: `RC Tech`,
         start_url: `/`,
         background_color: `#000`,
         theme_color: `#000`,
